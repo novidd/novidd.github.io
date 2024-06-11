802c1618deb4c8fc4aa1794688a697de92899a72
novidd <clm.pettersson@gmail.com>
2024-06-11 20:08:25 +0200

    update

---
+++ carousel.js
@@ -0,0 +1,28 @@
+// PART 2 VIDEO TIMESTAMP: 26:18
+
+const track = document.querySelector(".carousel__track");
+const slides = Array.from(track.children);
+const nextButton = document.querySelector(".carousel__button--right");
+const prevButton = document.querySelector(".carousel__button--left");
+const dotsNav = document.querySelector(".carousel__nav");
+const dots = Array.from(dotsNav.children);
+
+const slideWidth = slides[0].getBoundingClientRect().width;
+
+// Arrange the slides next to one another
+const setSlidePosition = (slide, index) => {
+  slide.style.left = slideWidth * index + "px";
+};
+slides.forEach(setSlidePosition);
+
+// When I click left, move slides to the left
+
+// When I click right, move slides to the right
+nextButton.addEventListener("click", e => {
+  const currentSlide = track.querySelector(".current-slide");
+  const nextSlide = currentSlide.nextElementSibling;
+  const amountToMove = nextSlide.style.left;
+
+  // Move to the next slide
+  track.style.transform = "translateX(-" + amountMove + ")";
+})
