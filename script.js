0cff6fc7bb945b710e11eb9516f474eb1e47bd6d
novidd <clm.pettersson@gmail.com>
2024-10-31 15:29:17 +0100

    ham complete

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-var animation = bodymovin.loadAnimation({
-  conatiner: document.getElementsById("topographicmap"),
-  renderer: "svg",
-  loop: true,
-  autoplay: true,
-  path: "data.json"
-})
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    var x =  document.getElementById("ham-menu");
+    if (x.style.display === "flex") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "flex";
+    }
+} 
